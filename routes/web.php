172df98496c1839c11353aa1c70a93c08920e3ae
pearlgw <gayuhterluv@gmail.com>
2025-04-04 13:12:08 +0700

    materi ke 4 logic

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::get('/dokter/dashboard', function () {
 
 // Jika menggunakan controller
 Route::get('/dokter/obat', [ObatController::class, 'index']);
+Route::get('/dokter/obat/create', [ObatController::class, 'create']);
+Route::post('/dokter/obat', [ObatController::class, 'store']);
+Route::get('/dokter/obat/{id}/edit', [ObatController::class, 'edit']);
+Route::put('/dokter/obat/{id}', [ObatController::class, 'update']);
+Route::delete('/dokter/obat/{id}', [ObatController::class, 'destroy']);
 
 Route::get('/pasien/dashboard', function () {
     return view('pasien.index');
